52ad95e0de4a25e4408a5ccef921ea6a80baaf3d
Wim Van Laer <van.laer.wim@gmail.com>
2018-12-03 20:08:57 +0100

    day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -9,15 +9,45 @@ import (
 func main() {
 	args := os.Args[1:]
 
-	sum := 0
-
+	is := make([]int, 0)
 	for _, a := range args {
 		i, err := strconv.Atoi(a)
 		if err != nil {
 			panic(err)
 		}
-		sum += i
+		is = append(is, i)
 	}
 
-	fmt.Println(sum)
+	fmt.Println("Part 1:", solvePart1(is))
+	fmt.Println("Part 2:", solvePart2(is))
+}
+
+func solvePart1(is []int) (sum int) {
+	for _, i := range is {
+		sum += i
+	}
+	return sum
+}
+
+func solvePart2(is []int) (duplicate int) {
+	sums := make(map[int]struct{})
+	sums[0] = struct{}{}
+	sum := 0
+
+	for _, i := range is {
+		sum += i
+		sums[sum] = struct{}{}
+	}
+
+	for {
+		for _, i := range is {
+			sum += i
+
+			if _, ok := sums[sum]; ok {
+				return sum
+			} else {
+				sums[sum] = struct{}{}
+			}
+		}
+	}
 }
