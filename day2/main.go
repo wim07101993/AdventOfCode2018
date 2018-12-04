c8525cad30f7e01b54e810ef2846391edd90bf42
wim van laer <van.laer.wim@gmail.com>
2018-12-04 12:50:04 +0100

    day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	args := os.Args[1:]
 
 	fmt.Println("Part 1:", solvePart1(args))
+	fmt.Println("Part 2:", solvePart2(args))
 }
 
 func solvePart1(ids []string) int {
@@ -40,4 +42,39 @@ func solvePart1(ids []string) int {
 	}
 
 	return doubles * triples
+}
+
+func solvePart2(ids []string) string {
+	for i := 0; i < len(ids); i++ {
+		for j := i + 1; j < len(ids); j++ {
+			if b, s := hasOnlyOneLetterDifferent(ids[i], ids[j]); b {
+				return s
+			}
+		}
+	}
+	return ""
+}
+
+func hasOnlyOneLetterDifferent(id1 string, id2 string) (bool, string) {
+	foundDiff := false
+	equalLetters := ""
+	for i := 0; i < len(id1); i++ {
+		if id1[i] != id2[i] {
+			if foundDiff {
+				return false, ""
+			} else {
+				foundDiff = true
+
+				builder := strings.Builder{}
+				for j := 0; j < len(id1); j++ {
+					if i != j {
+						builder.WriteRune(rune(id1[j]))
+					}
+				}
+				equalLetters = builder.String()
+			}
+		}
+	}
+
+	return foundDiff, equalLetters
 }
